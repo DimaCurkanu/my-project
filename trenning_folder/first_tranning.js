0df6648226f74076c8fb4e287ab7b92da702b763
DimaCurkanu <kdgrig@gmail.com>
2023-07-01 15:07:21 -0500

    largest integer k such that 3^k

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -296,3 +296,23 @@ function factorial(n){
   return answer;
 }
  console.log(factorial(4));
+
+ ////////////// 
+// Given a positive integer N, return the largest integer k such that 3^k < N.
+// For example,
+// largest_power(3) == 0
+// largest_power(4) == 1
+// You may assume that the input to your function is always a positive integer
+ 
+function largestPower(n){
+  
+  let k = 0;
+  let s = 3 ** k;
+  while ( s < n){
+    s = s * 3;
+  k++;
+  console.log(s);
+  }
+  return k-1 // return the largest integer k such that 3^k < n
+}
+console.log(largestPower(3));
