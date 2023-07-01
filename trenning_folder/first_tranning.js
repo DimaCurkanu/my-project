4a8f11fcb01cb3fc20f0d967fcff8877ee64d61e
DimaCurkanu <kdgrig@gmail.com>
2023-07-01 17:27:00 -0500

    Get the next prime number

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -337,3 +337,20 @@ function isVeryEvenNumber(n) {
 return n % 2 === 0 ? true : false;
 }
 console.log(isVeryEvenNumber(371));
+///////////////////
+// Get the next prime number!
+// You will get a numbern (>= 0) and your task is to find the next prime number.
+// Make sure to optimize your code: there will numbers tested up to about 10^12.
+
+function nextPrime(n){
+  if (++n < 2) {
+    return 2
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i == 0) {
+      return nextPrime(n)
+    }
+  }
+  return n
+}
+console.log(nextPrime(12342355));
