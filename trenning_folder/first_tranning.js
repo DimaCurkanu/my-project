e70c279a3b5fe1374560c80867b7e12f3f4ccc08
DimaCurkanu <kdgrig@gmail.com>
2023-07-03 12:40:27 -0500

    sum of all the integers between and including

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -529,3 +529,25 @@ var summation = function (num) {
   return sum
 }
 console.log(summation(8));
+
+// Given two integers a and b, which can be positive or negative, 
+// find the sum of all the integers between and including them and return it. 
+// If the two numbers are equal return a or b.
+// Note: a and b are not ordered!
+
+function getSum(a, b)
+{
+  if ( b <= a) {
+    let sum = 0;
+  for (i = b; i <= a; i++){
+    sum += i;
+  }
+  return sum;
+  } 
+  let sum = 0;
+  for (i = a; i <= b; i++){
+    sum += i;
+  }
+   return sum
+}
+console.log(getSum(0, 4));
