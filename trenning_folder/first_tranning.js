006e3990cba64e0b7f8f85e68e6dbece98a413eb
DimaCurkanu <kdgrig@gmail.com>
2023-07-22 20:37:06 -0500

    squareOrSquareRoot

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -1367,3 +1367,23 @@ function squares(x, n) {
   }
   return arr
 }
+//Write a method, that will get an integer array as parameter and will 
+//process every number from this array.
+//Return a new array with processing every number of the input-array like this:
+//If the number has an integer square root, take this, otherwise square the number.
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
+function squareOrSquareRoot(array) {
+  let a = [];
+  for(i = 0; i < array.length; i++) {
+    if (Math.floor(Math.sqrt(array[i])) == Math.ceil(Math.sqrt(array[i]))) {
+      array[i] = Math.sqrt(array[i])
+    } else { array[i] = Math.pow(array[i], 2)
+    }
+    a.push(array[i])
+  }
+  return a;  
+}
+console.log(squareOrSquareRoot([ 4, 3, 9, 7, 2, 1 ]));
