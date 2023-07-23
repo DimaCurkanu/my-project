fb7ebb859b933105abfbe6866e5faffcc9c837bf
DimaCurkanu <kdgrig@gmail.com>
2023-07-22 19:36:10 -0500

    more interest solution

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -1358,3 +1358,12 @@ for (i = 1; i < n; i++) {
 return arr 
 }
 console.log(squares(10, -4));
+/////////////// more interest solution-->>
+function squares(x, n) {
+  let arr = [];
+  if(n === 0 || n < 0) return arr;
+  for(let i = x; arr.length < n; i*=i) {
+      arr.push(i);
+  }
+  return arr
+}
