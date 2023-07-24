5c4a48232eb38dcb317654b028eca2ef0dd4b433
DimaCurkanu <kdgrig@gmail.com>
2023-07-24 15:39:59 -0500

    findNextSquare(sq)

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -1439,3 +1439,19 @@ var isSquare = function(n){
 function isSquare(n) {
   return Math.sqrt(n) % 1 === 0;
 }
+///////
+// Complete the findNextSquare method that finds the next integral perfect 
+// square after the one passed as a parameter. Recall that an integral perfect 
+// square is an integer n such that sqrt(n) is also an integer.
+
+// If the parameter is itself not a perfect square then -1 should be returned. 
+// You may assume the parameter is non-negative.
+function findNextSquare(sq) {
+  // Return the next square if sq is a perfect square, -1 otherwise
+  return Math.sqrt(sq) % 1 === 0 ? (Math.sqrt(sq) + 1) ** 2 : -1; // ====> my solution
+}
+// site solution:
+function findNextSquare(sq) {
+  let r = Math.sqrt(sq)
+  return r % 1 ? -1 : ++r * r
+}
