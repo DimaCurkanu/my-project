940d4b0e0c57c34c13f67c9268e773cbaf2350f9
DimaCurkanu <kdgrig@gmail.com>
2023-07-24 15:08:00 -0500

    isSquare

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -1425,3 +1425,17 @@ function powersOfTwo(n){
   }
   return a
   }
+  //////////
+//   Given an integral number, determine if it's a square number:
+
+// In mathematics, a square number or perfect square is an integer 
+// that is the square of an integer; in other words, it is the product 
+// of some integer with itself.
+// my solution:
+var isSquare = function(n){
+  return Math.floor(Math.sqrt(n)) == Math.ceil(Math.sqrt(n))? true : false; 
+}
+//// best solution: !!!!!!!!!!!!!!!!!!!!!!
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
