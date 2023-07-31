42bf20f7a024529a786612f4ce1a2069b5f07165
DimaCurkanu <kdgrig@gmail.com>
2023-07-31 11:02:54 -0500

    coinCombo (cents)

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -1588,3 +1588,33 @@ function race(v1, v2, g) {
   
   return [h, m, s];
 }
+//////
+// The function takes cents value (int) and needs to return the minimum number of coins 
+// combination of the same value.
+
+// The function should return an array where
+// coins[0] = pennies ==> $00.01
+// coins[1] = nickels ==> $00.05
+// coins[2] = dimes ==> $00.10
+// coins[3] = quarters ==> $00.25
+
+// So for example:
+// coinCombo(6) --> [1, 1, 0, 0]
+function coinCombo (cents) {
+  const quarters = Math.trunc(cents / 25);
+  const dimes = Math.trunc((cents - quarters*25)/10);
+  const nickels = Math.trunc((cents - quarters*25 - dimes*10)/5);
+  const pennies = Math.trunc((cents - quarters*25 - dimes*10 - nickels*5)/1);
+	return [pennies, nickels, dimes, quarters];
+}
+console.log(coinCombo (36));
+///////через for:::::
+function coinCombo(cents) {
+  let coins = [1,5,10,25];
+  let combo = [0,0,0,0];
+  for (let i = 3; i >= 0; i--) {
+      combo[i] = Math.floor(cents / coins[i]);
+      cents -= combo[i] * coins[i];
+  }
+  return combo;
+}
