c37e01fda9fdde89227d9eb3fab96e7146c8900e
DimaCurkanu <kdgrig@gmail.com>
2023-08-09 12:21:06 -0500

    array.length

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -1868,3 +1868,48 @@ function formatArray(arr){
       arr.forEach(el=> glasses += parseInt(el));
       return glasses * 200 + ' ml';
     }
+/////////----------------------------------------
+
+    function getLength(arr){
+      //return length of arr
+      return arr.length;
+    }
+    console.log(getLength([1,2,3]));
+
+    function getFirst(arr){
+      //return the first element of arr
+      return arr[0];
+    }
+    console.log(getFirst([1,2,3]));
+
+    function getLast(arr){
+      //return the last element of arr
+      return arr[arr.length-1];
+    }
+    console.log(getLast([1,2,3]));
+
+    function pushElement(arr){
+      var el=1;
+      //push el to arr
+      arr.push(el);
+      return arr.length;
+    }
+    console.log(pushElement([1,2,3]));
+
+    function popElement(arr){
+      //pop an element from arr
+       arr.pop();
+      return arr.length;
+    }
+    console.log(popElement([1,2,3]));
+
+    //========== another solution:::::::::
+    const getLength = array => array.length;
+
+const getFirst = array => array[0];
+
+const getLast = array => array[array.length - 1];
+
+const pushElement = array => array.concat(0);
+
+const popElement = array => array.splice(0, array.length - 1);
