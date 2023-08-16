f84338d5b67df198ffd4ed2add22217ee600d378
DimaCurkanu <kdgrig@gmail.com>
2023-08-16 10:39:54 -0500

    Is every value in the array an array?

--- trenning_folder/first_tranning.js
+++ trenning_folder/first_tranning.js
@@ -1988,3 +1988,19 @@ console.log(typeof(type([])));
 function type(value) {
   return {}.toString.call(value).slice(8, -1).toLowerCase();
 }
+/////
+// Is every value in the array an array?
+// This should only test the second array dimension of the array. 
+// The values of the nested arrays don't have to be arrays.
+// Examples:
+// [[1],[2]] => true
+// ['1','2'] => false
+// [{1:1},{2:2}] => false
+///===========================>>>>>>>>>>>>>>>>>>>>>>>>>>
+const arrCheck = value => {
+  for (i=0; i < value.length; i++) {
+    if({}.toString.call(value[i]) !== '[object Array]') return false;
+  }
+  return true
+}
+console.log(arrCheck([[1], [2], 66, [4]]));
